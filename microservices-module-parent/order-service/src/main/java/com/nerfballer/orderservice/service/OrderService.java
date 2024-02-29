c51ca7844feb2636679a29c61da27a9c16e34ac4
David Mizativan-Ortega <dortega5185@gmail.com>
2024-02-29 01:03:04 -0600

    Finished working on the Discovery Server, tested the app without the discovery server running and it returned an error as intended. Initializing another maven module: API-Gateway to handle all requests from users as well as take care of authentication, security, load balancing, and SSL Termination.

--- microservices-module-parent/order-service/src/main/java/com/nerfballer/orderservice/service/OrderService.java
+++ microservices-module-parent/order-service/src/main/java/com/nerfballer/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
 	
 	// constructor injection
 	private final OrderRepository orderRepository;
-	private final WebClient webClient;
+	private final WebClient.Builder webClientBuilder;
 	
 	public void placeOrder(OrderRequest orderRequest) {
 		Order order = new Order();
@@ -44,8 +44,8 @@ public class OrderService {
 		
 		
 		// Call Inventory Service, and place order if product is in stock
-		InventoryResponse[] inventoryResponseArray = webClient.get()
-			.uri("http://localhost:8082/api/inventory",
+		InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+			.uri("http://inventory-service/api/inventory",
 					uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
 			.retrieve()
 			.bodyToMono(InventoryResponse[].class)
