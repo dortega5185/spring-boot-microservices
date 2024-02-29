85d70180fb3b8f9d83984fa16ba01bf9add9e07e
David Mizativan-Ortega <dortega5185@gmail.com>
2024-02-29 01:11:14 -0600

    Delete order-service directory
    this was the original state of the order-service before I made a module directory for the services to live in and communicate with each other.

--- order-service/src/main/java/com/nerfballer/orderservice/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nerfballer.orderservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nerfballer.orderservice.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
